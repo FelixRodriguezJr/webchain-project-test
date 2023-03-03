6220e55966afb2e2b561e2a68464473d8a5cd03c
Alec Joseph <47369385+KingJohn12@users.noreply.github.com>
2023-03-02 21:25:12 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const dataFile = path.join(__dirname, "data.json");
+
+
+app.use(express.urlencoded({extended:true}));
+
+
+app.use((req, res, next) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    next();
+
+});
+
+
+app.get("/poll", async(req, res) => {
+    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+
+    const totalVotes = Object.values(data).reduce((total,n) => total +=n, 0);
+
+    data = Object.entries(data).map(([label,votes]) => {
+        return {
+            label, 
+            amountofVotes: (((100 * votes) /totalVotes) || 0).toFixed(0)
+        }
+       
+
+    });
+
+
+
+    res.json(data);
+
+});
+
+app.post("/poll", async(req,res) => {
+
+    const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+
+    data[req.body.add]++;
+
+    await fs.writeFile(dataFile, JSON.stringify(data))
+
+    res.end();
+});
+
+app.listen(3000, () => console.log("Server is running......."));
